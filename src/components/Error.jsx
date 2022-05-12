1046c16d789717910f897e237061bad48f8fee1f
Carl7 <carlosmiguel7cb@gmail.com>
2022-05-12 18:52:33 -0500

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+function Error({children}) {
+  return (
+    <div className='bg-red-800 text-white text-center uppercase font-bold p-3 mb-3 rounded-md'>
+        {children}
+    </div>
+  )
+}
+
+export default Error
